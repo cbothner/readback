f0746183d54104c8edc1309c21e886d3a69c963f
Cameron Bothner <cbothner@umich.edu>
2015-05-15 01:18:10 -0400

    Little changes.  Let's go to bed.

--- app/assets/javascripts/playlist.js
+++ app/assets/javascripts/playlist.js
@@ -1,7 +1,7 @@
 $(document).on("ready page:load", function() {
   //$(document.body).scrollTop($("#next_up").offset().top);
   if (location.pathname === "/") {
-    location.hash = "#next_up";
+    location.hash = "#now";
     $("[autofocus]").focus();
   }
 });
