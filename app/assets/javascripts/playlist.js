c1004599fd5c72d21ac9d7af54bb30759c5c9acf
Cameron Bothner <cbothner@umich.edu>
2015-05-15 00:51:34 -0400

    Changed name, fixed scrolling, autofocus

--- app/assets/javascripts/playlist.js
+++ app/assets/javascripts/playlist.js
@@ -1,3 +1,7 @@
 $(document).on("ready page:load", function() {
-  $(document.body).scrollTop($("#next_up").offset().top);
+  //$(document.body).scrollTop($("#next_up").offset().top);
+  if (location.pathname === "/") {
+    location.hash = "#next_up";
+    $("[autofocus]").focus();
+  }
 });
