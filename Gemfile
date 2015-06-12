cf743ae5eff6f5d016f4d872a78175e509045da3
Cameron Bothner <cbothner@umich.edu>
2015-06-12 18:47:14 -0400

    Set up Rspec and FactoryGirl

--- Gemfile
+++ Gemfile
@@ -44,6 +44,14 @@ group :development, :test do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+
+  gem 'rspec-rails'
+  gem 'factory_girl_rails', require: false
+end
+
+group :test do
+  gem 'faker'
+  gem 'shoulda-matchers'
 end
 
 #gem 'devise'
--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,16 @@ module Playlist
     config.active_record.raise_in_transactional_callbacks = true
 
     config.autoload_paths += %W(#{config.root}/lib)
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        :fixtures => true,
+        :view_specs => false,
+        :helper_specs => false,
+        :routing_specs => false,
+        :controller_specs => true,
+        :request_specs => true
+      g.fixture_replacement :factory_girl, :dir => "spec/factories"
+    end
   end
 end
