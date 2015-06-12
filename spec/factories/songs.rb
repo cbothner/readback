1ba1c8f1e657f6bfdba3156246b2ddf90886ddf6
Cameron Bothner <cbothner@umich.edu>
2015-06-12 18:47:48 -0400

    Songs factory and spec test

---
+++ spec/factories/songs.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+
+  factory :song do
+    name { Faker::Commerce.product_name }
+    artist { Faker::App.name }
+    album { Faker::Commerce.department }
+    label { Faker::Company.name + " Records" }
+    year { Faker::Number.between 1950, 2015 }
+    request { [true, false].sample }
+  end
+
+end
---
+++ spec/models/song_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Song, type: :model do
+
+  it {should validate_presence_of :name}
+
+end
