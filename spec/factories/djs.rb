f74c59cd978d7f8272b3b7daaf76643441409815
Cameron Bothner <cbothner@umich.edu>
2015-06-13 00:49:39 -0400

    Fixed DJ factory, created Shows factory

--- spec/factories/djs.rb
+++ spec/factories/djs.rb
@@ -5,11 +5,19 @@ FactoryGirl.define do
     transient do
       trainee false
       nighttime false
+      um_affiliated true
     end
 
     name { Faker::Name.name }
     phone { Faker::PhoneNumber.phone_number }
     email { Faker::Internet.email }
+    um_affiliation {
+      um_affiliated ? Dj::AFFILIATED_UM_AFFILIATIONS.sample : Dj::UNAFFILIATED_UM_AFFILIATIONS
+    }
+    um_dept { ["LSA Linguistics", "LSA Physics", "COE Computer Science"]
+      .sample if um_affiliated }
+    umid { Faker::Number.between(1111_1111, 9999_9999) if um_affiliated }
+    statement { Faker::Lorem.paragraph unless um_affiliated }
 
   end
 
---
+++ spec/factories/shows.rb
@@ -0,0 +1,40 @@
+FactoryGirl.define do
+
+  trait :show do
+
+    # semester
+    weekday { Faker::Number.between 0, 6 }
+    beginning { Show::UNIMPORTANT_DATE.change(hour: Faker::Number.between(6, 20)) }
+    ending { beginning + Faker::Number.between(1, 3).hours }
+
+    trait :nighttime do
+      beginning { Show::UNIMPORTANT_DATE.change hour: [0,3].sample }
+      ending { beginning + 3.hours }
+    end
+
+  end
+
+  factory :freeform_show, traits: [:show] do
+    dj
+    name = "Freeform"
+  end
+
+  factory :specialty_show, traits: [:show] do
+    name { ["Radiozilla", "Dead White Guys", "What It Is"].sample }
+
+    factory :specialty_show_with_djs do
+      transient { dj_count 4 }
+
+      after(:create) do |show, evaluator|
+        create_list(:dj, evaluator.dj_count, specialty_show: specialty_show)
+      end
+
+    end
+  end
+
+  factory :talk_show, traits: [:show] do
+    dj
+    name = "Talk Show"
+  end
+
+end
