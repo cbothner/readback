a2c7dea2a7e391ee92cd36962a2016e593892db2
Cameron Bothner <cbothner@umich.edu>
2015-07-06 20:59:19 -0400

    Shows a little circular button that let's the user quickly jump to #NOW.

--- app/assets/javascripts/playlist.js
+++ app/assets/javascripts/playlist.js
@@ -4,4 +4,27 @@ $(document).on("ready page:load", function() {
     location.hash = "#now";
     $("[autofocus]").focus();
   }
+  function showNowButton() {
+    $('#now-button').fadeIn('fast');
+  }
+
+  function enableScroll() {
+    $(window).scroll(function(){
+      var diff = Math.abs($(window).scrollTop() - $("#now").offset().top);
+      console.log( diff );
+      if (diff > 100) {
+        showNowButton();
+        $(window).off("scroll")
+      }
+    });
+  }
+  enableScroll();
+
+  $('#now-button').on("click touchend", function() {
+    event.preventDefault();
+    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 'fast', function() {
+      $('#now-button').hide();
+      enableScroll();
+    });
+  });
 });
