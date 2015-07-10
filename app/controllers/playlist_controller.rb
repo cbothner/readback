4fb3e95e126589575f383f53103c35805a7c12b4
Cameron Bothner <cbothner@umich.edu>
2015-07-09 23:51:29 -0400

    Fixes the mistaken non-showing of the starts of past shows.
    Since past shows are sorted according to their end time to allow for
    proper divisions between shows, the selection criteria also need to
    look for past endings, not beginnings.

--- app/controllers/playlist_controller.rb
+++ app/controllers/playlist_controller.rb
@@ -1,7 +1,7 @@
 class PlaylistController < ApplicationController
   def index
     songs = Song.includes(:episode).where(at: 6.hours.ago..Time.zone.now)
-    shows = Episode.includes(:dj, show: [:dj]).where(beginning: 6.hours.ago..4.hours.since)
+    shows = Episode.includes(:dj, show: [:dj]).where("beginning < '#{4.hours.since.utc}' and ending > '#{6.hours.ago.utc}'")
     @on_air = Episode.on_air
     signoff_instances = SignoffInstance.where(at: 6.hours.ago..4.hours.since)
 
