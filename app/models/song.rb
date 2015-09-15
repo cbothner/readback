812827702903b4fa3a7a9ee74ebcc448fe90876b
Cameron Bothner <cbothner@umich.edu>
2015-09-15 14:46:43 -0400

    Updates the Icecast metadata as a callback on song save.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,17 @@
 class Song < ActiveRecord::Base
   validates :name, :episode_id, presence: true
   belongs_to :episode
+
+  after_create :post_information_to_icecast
+
+  def post_information_to_icecast
+    %w(hd mid hi)
+      .each do |qual|
+      Kernel.system "curl --user admin:#{ENV[ICECAST_ADMIN_PASSWORD]}  \"http://floyd.wcbn.org:8000/admin/metadata?mount=/wcbn-#{qual}.mp3&mode=updinfo&song=#{Rack::Utils.escape metadata_string}\""
+    end
+  end
+
+  def metadata_string
+    "WCBN-FM: “#{name}” by #{artist} – on #{episode.show.name} with #{episode.dj}"
+  end
 end
