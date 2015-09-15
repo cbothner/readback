1318b4bfce0615cf479e3d984e726fbe1bc0f28e
Cameron Bothner <cbothner@umich.edu>
2015-09-15 01:30:33 -0400

    Fixes the playlist view for future shows past a certain amount of time.

--- app/controllers/playlist_controller.rb
+++ app/controllers/playlist_controller.rb
@@ -4,7 +4,8 @@ class PlaylistController < ApplicationController
     iframe = params[:from] == 'iframe'
 
     songs = Song.includes(:episode).where(at: 6.hours.ago..Time.zone.now)
-    episodes = Episode.includes(:dj, :songs, show: [:dj], trainee: [:episodes]).where("beginning < '#{4.hours.since.utc}' and ending > '#{6.hours.ago.utc}'")
+    episodes = Episode.includes(:dj, :songs, show: [:dj], trainee: [:episodes])
+      .where ending: 6.hours.ago..Time.zone.now, beginning: Time.zone.now..4.hours.since
     @on_air = Episode.on_air
     episodes -= [@on_air]
     signoff_instances = SignoffInstance.where(at: 6.hours.ago..4.hours.since)
