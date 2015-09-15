74380b5e0a5258b959eff6190eb0293f1b128376
Cameron Bothner <cbothner@umich.edu>
2015-09-15 05:23:40 -0400

    Actually send mails in production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,20 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { host: 'wcbn-readback.herokuapp.com' }
+  #config.action_mailer.default_url_options = { host: 'radio.wcbn.org' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'wcbn.org',
+    user_name:            ENV['readback_gmail_user'],
+    password:             ENV['readback_gmail_pass'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
