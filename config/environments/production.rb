90a88e640ca67e6a660bf7f3eb9b8cbb44e7c499
Cameron Bothner <cbothner@umich.edu>
2015-09-16 10:18:32 -0400

    Use SendGrid for email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,14 +80,14 @@ Rails.application.configure do
   #config.action_mailer.default_url_options = { host: 'radio.wcbn.org' }
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'gmail.com',
-    user_name:            ENV['readback_gmail_user'],
-    password:             ENV['readback_gmail_pass'],
-    authentication:       :login,
-    enable_starttls_auto: true
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
