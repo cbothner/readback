1faadfaff435c24c27031204265c04dc0b1d0db8
Cameron Bothner <cbothner@umich.edu>
2015-09-16 22:52:59 -0400

    Display host blocks and sub messages on Talk Show page.

--- app/helpers/episodes_helper.rb
+++ app/helpers/episodes_helper.rb
@@ -1,5 +1,5 @@
 module EpisodesHelper
-  def with(ep)
+  def with(ep, short:false)
     dj_name = link_to(ep.active_dj, ep.active_dj).html_safe
     case ep.show_type
     when "FreeformShow" then
@@ -7,7 +7,11 @@ module EpisodesHelper
     when "SpecialtyShow" then
       "with rotating host #{dj_name}".html_safe
     when "TalkShow" then
-      "a WCBN public affairs show#{" with your host, #{dj_name}".html_safe unless ep.show.dj.nil?}".html_safe
+      wcbn_pa = with = ""
+      wcbn_pa = "a WCBN public affairs show" unless short
+      your_or_guest = ep.subbed_for? ? "guest" : "your"
+      with = "with #{your_or_guest} host, #{dj_name}" unless ep.show.dj.nil?
+      raw wcbn_pa + with
     end
   end
 end
