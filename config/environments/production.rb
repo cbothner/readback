539f29a1a641a6b298a8c13de7bc6d0345a0b7d3
Cameron Bothner <cbothner@umich.edu>
2015-10-21 14:47:21 -0400

    Updates the default url options for emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { host: 'wcbn-readback.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'app.wcbn.org' }
   #config.action_mailer.default_url_options = { host: 'radio.wcbn.org' }
 
   config.action_mailer.delivery_method = :smtp
