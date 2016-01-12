9be8cfbe249967e1dd8cbede4756fa999e37b239
Cameron Bothner <cbothner@umich.edu>
2016-01-12 15:35:59 -0500

    Allows TalkShow hosts to edit their public affairs log after the show

--- app/authorizers/episode_authorizer.rb
+++ app/authorizers/episode_authorizer.rb
@@ -3,6 +3,8 @@ class EpisodeAuthorizer < OwnedModelAuthorizer
   def updatable_by?(user)
     return true if (resource == Episode.on_air)
 
+    return true if resource.notes.nil? && resource.show_type == "TalkShow"
+
     return true if user.can_update? resource.show
 
     super
