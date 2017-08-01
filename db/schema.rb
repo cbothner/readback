fb8948064b81f2287827bf33c02aee94aef88b21
Cameron Bothner <cameronbothner@gmail.com>
2017-08-01 10:25:15 -0400

    Changes djs.name to a NOT NULL column

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160821181009) do
+ActiveRecord::Schema.define(version: 20170801142332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "djs", force: :cascade do |t|
-    t.string   "name"
+    t.string   "name",                                  null: false
     t.string   "phone"
     t.string   "email",                  default: "",   null: false
     t.string   "um_affiliation"
