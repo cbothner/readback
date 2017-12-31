87f6adf8dde04db1c53e10be176d3474fb30aaa7
Cameron Bothner <cameronbothner@gmail.com>
2017-12-31 13:37:07 -0500

    Refactors SemesterClonerJob

--- app/jobs/semester_cloner_job.rb
+++ app/jobs/semester_cloner_job.rb
@@ -1,34 +1,51 @@
+# Clone a selection of shows from one semester into another
 class SemesterClonerJob < ApplicationJob
   queue_as :default
 
+  # @param show_types_to_copy [Hash<class_name: String, ids: Array<Number>>]
+  # @param into_semester [Semester]
   def perform(show_types_to_copy, into_semester:)
     show_types_to_copy.each do |type, ids|
-      clone_shows show_type: type, ids: ids, into_semester: into_semester
+      clone_shows_of_type type, ids: ids, into_semester: into_semester
     end
   end
 
   private
-  def clone_shows(show_type:, ids:, into_semester:)
+
+  def clone_shows_of_type(type, ids:, into_semester:)
     ids.each do |id|
-      old = show_type.camelize.singularize.constantize.find(id)
-
-      new = old.class.new(
-        old.attributes.slice *%w(name weekday dj_id coordinator_id topic description )
-      )
-      new.semester = into_semester
-
-      times_params = { weekday: old.times.first.wday,
-                       hour: old.times.first.hour, minute: old.times.first.min,
-                       duration: old.times.duration }
-      new.set_times times_params
-
-      # Save the show without validating time conflicts because the model
-      # semester ought to enforce that, and the check makes save slow.
-      if new.save(validate: false)  # Callback propagates
-        old.djs.each {|o| new.djs << o} if new.is_a? SpecialtyShow
-      else
-        raise "Clone Error"
-      end
+      clone_show(find_show(type, id), into_semester: into_semester)
     end
+  end
+
+  def find_show(type, id)
+    type.camelize.singularize.constantize.find(id)
+  end
+
+  def clone_show(show, into_semester:)
+    new_show = show.class.new(attributes_to_clone(show))
+    new_show.semester = into_semester
+    new_show.set_times time_params show
+
+    # Save the show without validating time conflicts because the model
+    # semester ought to enforce that, and the check makes save slow.
+    new_show.save(validate: false) # Callback propagates
+
+    copy_djs(new_show, from: show) if new_show.is_a? SpecialtyShow
+  end
+
+  def attributes_to_clone(show)
+    show.attributes.slice(
+      'name', 'weekday', 'dj_id', 'coordinator_id', 'topic', 'description'
+    )
+  end
+
+  def time_params(show)
+    { weekday: show.times.first.wday, hour: show.times.first.hour,
+      minute: show.times.first.min, duration: show.times.duration }
+  end
+
+  def copy_djs(new_show, from:)
+    from.djs.each { |o| new_show.djs << o }
   end
 end
