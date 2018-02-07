cbothner/readback

commit 668aa87a46bd1d657443e5371679707275a5ba42

Extracts icecast broadcast to a job

app/jobs/icecast_update_job.rb
@@ -1,0 +1,35 @@
+# frozen_string_literal: true
+
+# Set the given song’s information as the metadata of the icecast stream.
+class IcecastUpdateJob < ApplicationJob
+  queue_as :default
+
+  # @param [Song]
+  def perform(song)
+    return unless ENV['ICECAST_ADMIN_PASSWORD']
+
+    %w[hd mid hi].each do |qual|
+      Kernel.system(
+        'curl',
+        '--max-time', '0.5',
+        '--user', "admin:#{ENV['ICECAST_ADMIN_PASSWORD']}",
+        icecast_endpoint(qual, song)
+      )
+    end
+  end
+
+  private
+
+  def icecast_endpoint(qual, song)
+    mount = "/wcbn-#{qual}.mp3"
+    'http://floyd.wcbn.org:8000/admin/metadata' \
+      "?mount=#{mount}&song=#{metadata_string(song)}" \
+      '&mode=updinfo'
+  end
+
+  def metadata_string(song)
+    artist = " by #{song.artist}" unless artist.blank?
+    "WCBN-FM: “#{song.name}”#{artist} – " \
+      "on #{song.episode.show.name} with #{song.episode.dj}"
+  end
+end

.rubocop.yml
@@ -6,3 +6,5 @@ Lint/DefEndAlignment:
   AutoCorrect: true
 Lint/EndAlignment:
   AutoCorrect: true
+Style/ClassStructure:
+  Enabled: true

app/models/song.rb
@@ -1,30 +1,21 @@
+# frozen_string_literal: true
+
+# We love music
 class Song < ActiveRecord::Base
   belongs_to :episode
 
   validates :name, :episode_id, presence: true
   validates_datetime :at,
                      on_or_after: ->(t) { t.episode.beginning },
-                     before: ->(_t) { Time.zone.now }
+                     before: -> { Time.zone.now }
 
   after_commit { SongBroadcastJob.perform_later self }
-  after_create_commit :post_information_to_icecast
+  after_create_commit { IcecastUpdateJob.perform_later self }
 
-  scope :on_air, ->() { order(:at).last }
+  scope :on_air, -> { order(:at).last }
 
   def as_json(_options = {})
     super(only: %i[id name artist album label year
                    request new local at episode_id])
-  end
-
-  def post_information_to_icecast
-    return unless ENV['ICECAST_ADMIN_PASSWORD']
-    %w[hd mid hi]
-      .each do |qual|
-      Kernel.system "curl --max-time 0.5 --user admin:#{ENV['ICECAST_ADMIN_PASSWORD']}  \"http://floyd.wcbn.org:8000/admin/metadata?mount=/wcbn-#{qual}.mp3&mode=updinfo&song=#{Rack::Utils.escape metadata_string}\""
-    end
-  end
-
-  def metadata_string
-    "WCBN-FM: “#{name}”#{" by #{artist}" unless artist.blank?} – on #{episode.show.name} with #{episode.dj}"
   end
 end

commit 84540ec0db1718a7219923eaa4122419b1268521

Fixes IcecastUpdateJob to correctly URL escape the song information
668aa87 (#36) broke the icecast update because I forgot that the song
parameter needed to be URL escaped.

app/jobs/icecast_update_job.rb
@@ -23,7 +23,7 @@ class IcecastUpdateJob < ApplicationJob
   def icecast_endpoint(qual, song)
     mount = "/wcbn-#{qual}.mp3"
     'http://floyd.wcbn.org:8000/admin/metadata' \
-      "?mount=#{mount}&song=#{metadata_string(song)}" \
+      "?mount=#{mount}&song=#{Rack::Utils.escape metadata_string song}" \
       '&mode=updinfo'
   end
 