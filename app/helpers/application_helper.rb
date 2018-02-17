135680892e26aa8fa561f4e164e687ddd0efdd9b
Cameron Bothner <cameronbothner@gmail.com>
2018-02-16 20:22:01 -0500

    Redesigns the header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,15 @@
 module ApplicationHelper
   LATEX_SUBSTITUTIONS = { '“': '``', '”': "''", '‘': '`', '’': "'" }.freeze
 
+  # TODO
+  def page_path(*_args)
+    '/'
+  end
+
+  def events_path
+    '/'
+  end
+
   def parent_layout(layout)
     @view_flow.set(:layout, output_buffer)
     self.output_buffer = render(file: "layouts/#{layout}")
