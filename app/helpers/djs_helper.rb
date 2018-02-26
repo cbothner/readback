28a410a108d8bbb4282fff52b44d769c15edfc99
dctalbot <dctalbot@umich.edu>
2018-02-26 00:57:56 -0500

    Prototype profile photo upload w/ active storage

--- app/helpers/djs_helper.rb
+++ app/helpers/djs_helper.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 module DjsHelper
   def interest_link(name)
-    '<input type="button" class="interest-link" data-interest="'+name+'" value="'+
-      name+'" />'
+    '<input type="button" class="interest-link" data-interest="' + name + '" value="' +
+      name + '" />'
   end
 
   def interest_links(list)
-    list.map{ |i| interest_link i }.join.html_safe
+    list.map { |i| interest_link i }.join.html_safe
   end
 
   def needs_statement(trainee)
@@ -14,7 +16,7 @@ module DjsHelper
 
   def active_for_select(*selected_djs)
     active_djs = Dj.where(active: true).order(:name)
-    selected = selected_djs.flatten.map{ |dj| dj.try(:id) }
+    selected = selected_djs.flatten.map { |dj| dj.try(:id) }
     puts "selected = #{selected}"
     options_from_collection_for_select(active_djs, :id, :name, selected)
   end
@@ -29,15 +31,15 @@ module DjsHelper
   end
 
   def roles_for_roster(dj)
-    current_semester_shows = dj.shows.select{ |x| x.semester == Semester.current }
+    current_semester_shows = dj.shows.select { |x| x.semester == Semester.current }
     if current_semester_shows.empty?
-      "Sub Only"
+      'Sub Only'
     else
       types = current_semester_shows
-        .map {|x| x.class.name.underscore.humanize.titlecase}
-        .uniq
+              .map { |x| x.class.name.underscore.humanize.titlecase }
+              .uniq
       if types.length > 1
-        (types.map{ |x| x.split(' ').first } * ', ') + ' Shows'
+        (types.map { |x| x.split(' ').first } * ', ') + ' Shows'
       else
         types.first
       end
@@ -45,7 +47,12 @@ module DjsHelper
   end
 
   def profile_picture(dj)
-    content_tag :img, nil, class: ["profile-pic", ('robot' unless dj.has_custom_picture?)], src: dj.image_url
+    content_tag :img, nil, class: ['profile-pic', ('robot' unless dj.avatar.attached?)], src: dj_profile_url(dj)
   end
 
+  def dj_profile_url(dj)
+    return url_for(dj.avatar) if dj.avatar.attached?
+
+    dj.robot_picture_url
+  end
 end
