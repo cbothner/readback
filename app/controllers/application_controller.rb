8d4b3e31dfbbe139aba121098058fb0a22124710
Cameron Bothner <cameronbothner@gmail.com>
2018-03-04 12:11:10 -0500

    Refactor redesign layout so with_sidebar is not necessary

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :set_sidebar_variables
+
+  layout 'redesign'
+
   private
 
   def current_user
