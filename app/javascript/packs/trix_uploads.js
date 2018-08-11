85772aea6875d3d5064e27d4378b14e26fc78dbb
dctalbot <dctalbot@umich.edu>
2018-08-11 16:15:01 -0400

    send attachment url to trix editor after drag and drop, purge deleted images on update

--- app/javascript/packs/trix_uploads.js
+++ app/javascript/packs/trix_uploads.js
@@ -7,16 +7,11 @@ Trix.config.attachments.preview.caption = {
 
 const uploadAttachment = attachment => {
   const file = attachment.file
-
-  // your form needs the file_field direct_upload: true, which
-  //  provides data-direct-upload-url
-  // const url = input.dataset.directUploadUrl
   let upload = new DirectUpload(file, '/rails/active_storage/direct_uploads')
 
   upload.create((error, blob) => {
     if (error) {
-      // Handle the error
-      window.alert('Error uploading file from Trix')
+      console.log('Error uploading file from Trix')
     } else {
       // Add an appropriately-named hidden input to the form with a
       //  value of blob.signed_id so that the blob ids will be
@@ -27,122 +22,13 @@ const uploadAttachment = attachment => {
       hiddenField.setAttribute('value', blob.signed_id)
       document.querySelector('form').appendChild(hiddenField)
 
-      console.log(file)
-      console.log(blob)
-      console.log(blob.signed_id)
-
       return attachment.setAttributes({
-        url: '/rails/active_storage/disk/' + blob.signed_id + '/' + file.name,
-        href: '/rails/active_storage/disk/' + blob.signed_id + '/' + file.name,
+        url: '/rails/active_storage/blobs/' + blob.signed_id + '/' + blob.filename,
+        href: '/rails/active_storage/blobs/' + blob.signed_id + '/' + blob.filename,
       })
     }
   })
 }
-
-// const uploadAttachment = attachment => {
-//   const file = attachment.file;
-//
-//   // your form needs the file_field direct_upload: true, which
-//   //  provides data-direct-upload-url
-//   let upload = new DirectUpload(file, '/rails/active_storage/direct_uploads')
-//
-//   upload.create((error, blob) => {
-//     if (error) {
-//       // Handle the error
-//       window.alert('Error uploading file from Trix')
-//     }
-//     else {
-//       let form = new FormData;
-//       // form.append("key", key);
-//       form.append("Content-Type", file.type);
-//       // form.append("file", file);
-//
-//       // Add an appropriately-named hidden input to the form with a
-//       //  value of blob.signed_id so that the blob ids will be
-//       //  transmitted in the normal upload flow
-//       const hiddenField = document.createElement('input')
-//       hiddenField.setAttribute('type', 'hidden')
-//       hiddenField.name = 'dj[images][]'
-//       hiddenField.setAttribute('value', blob.signed_id)
-//       form.appendChild(hiddenField)
-//
-//       let xhr = new XMLHttpRequest;
-//       xhr.open("POST", host, true);
-//       xhr.upload.onprogress = (event) => {
-//         var progress;
-//         progress = event.loaded / event.total * 100
-//         return attachment.setUploadProgress(progress)
-//       }
-//       xhr.onload = () => {
-//         var href, url;
-//         if (xhr.status === 204) {
-//           url = href = host + key;
-//           return attachment.setAttributes({
-//             url: url,
-//             href: href
-//           })
-//         }
-//       }
-//       return xhr.send(form);
-//
-//     }
-//   })
-// }
-//
-//
-//   let key = createStorageKey(file);
-//   form = new FormData;
-//   form.append("key", key);
-//   form.append("Content-Type", file.type);
-//   form.append("file", file);
-//   xhr = new XMLHttpRequest;
-//   xhr.open("POST", host, true);
-//   xhr.upload.onprogress = function(event) {
-//     var progress;
-//     progress = event.loaded / event.total * 100;
-//     return attachment.setUploadProgress(progress);
-//   };
-//   xhr.onload = function() {
-//     var href, url;
-//     if (xhr.status === 204) {
-//       url = href = host + key;
-//       return attachment.setAttributes({
-//         url: url,
-//         href: href
-//       });
-//     }
-//   };
-//   return xhr.send(form);
-// };
-
-// const uploadAttachment = (attachment) => {
-//   const file = attachment.file;
-//   let form, xhr;
-//
-//   let key = createStorageKey(file);
-//   form = new FormData;
-//   form.append("key", key);
-//   form.append("Content-Type", file.type);
-//   form.append("file", file);
-//   xhr = new XMLHttpRequest;
-//   xhr.open("POST", host, true);
-//   xhr.upload.onprogress = function(event) {
-//     var progress;
-//     progress = event.loaded / event.total * 100;
-//     return attachment.setUploadProgress(progress);
-//   };
-//   xhr.onload = function() {
-//     var href, url;
-//     if (xhr.status === 204) {
-//       url = href = host + key;
-//       return attachment.setAttributes({
-//         url: url,
-//         href: href
-//       });
-//     }
-//   };
-//   return xhr.send(form);
-// };
 
 // Listen for the Trix attachment event to trigger upload
 addEventListener('trix-attachment-add', event => {
