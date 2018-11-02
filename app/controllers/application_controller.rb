c48bb764dddacef3541cc08d574fd3b9e502600f
Cameron Bothner <cameronbothner@gmail.com>
2018-11-01 21:42:26 -0400

    Use Sentry to catch Rails exceptions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,22 @@
+# frozen_string_literal: true
+
 require 'csv'
 
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :set_raven_context
+
   private
+
   def current_user
     current_dj || current_playlist_editor
+  end
+
+  def set_raven_context
+    Raven.user_context(
+      email: current_dj.email, playlist_editor: playlist_editor_signed_in?
+    )
+    Raven.extra_context(params: params.to_unsafe_h, url: request.url)
   end
 end
---
+++ config/initializers/sentry.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+Raven.configure do |config|
+  config.dsn = 'https://7eefb3c4a9dc4774ad70d87df9fed130:f57c599beeef4bcfa966a384858ff44c@sentry.io/1314257'
+end
