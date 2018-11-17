5ba0664de65dfd409e1ba480fbfd3546be72fc70
dctalbot <dctalbot@umich.edu>
2018-11-17 11:20:45 -0500

    focus styles for chrome

--- app/javascript/player/Volume.jsx
+++ app/javascript/player/Volume.jsx
@@ -4,6 +4,7 @@
  */
 
 import styled from 'styled-components'
+import { rgba } from 'polished'
 
 const Volume = styled.input.attrs({ type: 'range', min: 0, max: 10 })`
   writing-mode: bt-lr; /* IE */
@@ -21,6 +22,18 @@ const Volume = styled.input.attrs({ type: 'range', min: 0, max: 10 })`
     -webkit-box-shadow: none;
     -moz-box-shadow: none;
     box-shadow: none;
+  }
+
+  .tab-focus &:focus {
+
+    &::-webkit-slider-thumb,
+    input[type='range' i]::-webkit-media-slider-thumb, -moz-range-thumb {
+      box-shadow: 0 0 0px 1px ${props => props.theme.blue};
+    }
+
+    box-shadow: 0 0 9px 2px #ffffffa6;
+    border: 1px solid ${props => props.theme.blue};
+    
   }
 
   /* CHROME */
