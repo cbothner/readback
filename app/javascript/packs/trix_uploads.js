b5c4770ff28d9bfa7dac86d83494fe534b9d739c
Cameron Bothner <cameronbothner@gmail.com>
2018-11-17 12:58:23 -0500

    Look up model name when creating hidden input for trix upload

--- app/javascript/packs/trix_uploads.js
+++ app/javascript/packs/trix_uploads.js
@@ -10,38 +10,57 @@ const uploadAttachment = attachment => {
   const file = attachment.file
   let upload = new DirectUpload(file, '/rails/active_storage/direct_uploads')
 
-  upload.create((error, blob) => {
-    if (error) {
-      console.log('Error uploading file from Trix')
-    } else {
-      // Add an appropriately-named hidden input to the form with a
-      //  value of blob.signed_id so that the blob ids will be
-      //  transmitted in the normal upload flow
-      const hiddenField = document.createElement('input')
-      hiddenField.setAttribute('type', 'hidden')
-      hiddenField.name = 'dj[images][]'
-      hiddenField.setAttribute('value', blob.signed_id)
-      document.querySelector('form').appendChild(hiddenField)
+  return new Promise((resolve, reject) => {
+    upload.create((error, blob) => {
+      if (error) {
+        reject(new Error('Error uploading file from Trix'))
+      } else {
+        attachment.setAttributes({
+          url:
+            '/rails/active_storage/blobs/' +
+            blob.signed_id +
+            '/' +
+            encodeURIComponent(blob.filename),
+        })
 
-      return attachment.setAttributes({
-        url:
-          '/rails/active_storage/blobs/' +
-          blob.signed_id +
-          '/' +
-          encodeURIComponent(blob.filename),
-      })
-    }
+        resolve(blob.signed_id)
+      }
+    })
   })
+}
+
+function getAssociatedInput (trixEditor) {
+  return document.getElementById(trixEditor.getAttribute('input'))
+}
+
+function getHiddenInputName (associatedInput) {
+  const [model] = associatedInput.getAttribute('name').split('[')
+  return `${model}[images][]`
+}
+
+function addHiddenInput (trixEditor, blobId) {
+  const associatedInput = getAssociatedInput(trixEditor)
+
+  const hiddenField = document.createElement('input')
+  hiddenField.setAttribute('type', 'hidden')
+  hiddenField.name = getHiddenInputName(associatedInput)
+  hiddenField.setAttribute('value', blobId)
+
+  associatedInput.insertAdjacentElement('afterend', hiddenField)
 }
 
 // Listen for the Trix attachment event to trigger upload
 // If Trix editor has prevent-uploads data attribute, disallow upload
 addEventListener('trix-attachment-add', event => {
-  if (event.target.dataset.trixPreventUploads) {
+  const trixEditor = event.target
+
+  if (trixEditor.dataset.trixPreventUploads) {
     return event.preventDefault()
   }
   const attachment = event.attachment
   if (attachment.file) {
-    return uploadAttachment(attachment)
+    return uploadAttachment(attachment).then(blobId => {
+      addHiddenInput(trixEditor, blobId)
+    })
   }
 })
