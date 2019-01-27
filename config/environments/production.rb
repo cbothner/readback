66f637e6af7330fe931a9c4bfa14e939795fbd49
Cameron Bothner <cameronbothner@gmail.com>
2019-01-27 14:39:30 -0500

    Change smtp domain to wcbn.org

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
-    domain: 'app.wcbn.org',
+    domain: 'wcbn.org',
     address: 'smtp.sendgrid.net',
     port: 587,
     authentication: :plain,
