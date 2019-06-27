065834ad4c2104ec78ff87c565f91897f8a88994
Cameron Bothner <cameronbothner@gmail.com>
2019-06-27 14:49:08 -0400

    Fix flaky fullfilled own request error (#91)
    Relying on ActiveModel::Dirty is flaky. Maybe it’s a race condition.
    Maybe it’s about threads. IDK but sending the confirmation email from
    SubRequest::Fulfillment, after storing asking and fulfilling djs
    explicitly, instead of relying on the previous change record in the
    callback to SubRequest#after_save should probably fix it.

--- app/mailers/sub_request_mailer.rb
+++ app/mailers/sub_request_mailer.rb
@@ -1,46 +1,45 @@
-class SubRequestMailer < ApplicationMailer
+# frozen_string_literal: true
 
-  RADIO_NAMES = %w(radionauts radioteers radiologists radioland radioers
-  radioceans radiocopters radiofish radios radioists radiophones radioptics
-  radiologizers radiotizers radiosmiths radioramas)
+class SubRequestMailer < ApplicationMailer
+  RADIO_NAMES = %w[radionauts radioteers radiologists radioland radioers
+                   radioceans radiocopters radiofish radios radioists
+                   radiophones radioptics radiologizers radiotizers radiosmiths
+                   radioramas].freeze
 
   def request_of_group(sub_request)
     @sub_request = sub_request
     @dj = @sub_request.episode.dj
 
     mail to: @sub_request.requested_djs.map(&:name_and_email),
-      subject: "#{@sub_request.episode.dj} asked YOU to sub #{@sub_request.for}!"
+         subject: "#{@sub_request.episode.dj} asked YOU to sub #{@sub_request.for}!"
   end
 
   def request_of_all(sub_request)
     @sub_request = sub_request
     @dj = @sub_request.episode.dj
 
-    mail to: "Radio Free Ann Arbor <rfaa-mod@umich.edu>",
-      subject: "#{@sub_request.episode.dj} needs a sub #{@sub_request.for}!"
+    mail to: 'Radio Free Ann Arbor <rfaa-mod@umich.edu>',
+         subject: "#{@sub_request.episode.dj} needs a sub #{@sub_request.for}!"
   end
 
   def fulfilled(sub_request, asking_dj: nil)
     @sub_request = sub_request
     @fulfilling_dj = @sub_request.episode.dj
-    @asking_dj = if asking_dj.nil?
-                   @sub_request.episode.show.dj
-                 else
-                   asking_dj
-                 end
+    @asking_dj = asking_dj
+    @asking_dj ||= @sub_request.episode.show.dj
 
     attach_ical_event
 
     mail to: @asking_dj.name_and_email,
-      cc: @fulfilling_dj.name_and_email,
-      subject: "#{@fulfilling_dj} has taken your sub request #{@sub_request.for}"
+         cc: @fulfilling_dj.name_and_email,
+         subject: "#{@fulfilling_dj} has taken your sub request #{@sub_request.for}"
   end
 
   def unfulfilled(sub_request)
     @sub_request = sub_request
     @dj = @sub_request.episode.dj
 
-    mail to: @dj.name_and_email, subject: "Your sub request for tomorrow is still unclaimed!"
+    mail to: @dj.name_and_email, subject: 'Your sub request for tomorrow is still unclaimed!'
   end
 
   def reminder(sub_request)
