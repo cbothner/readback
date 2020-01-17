dc9542f024d2f0c6b2f281de9210d82b65fb8821
dctalbot <dctalbot@umich.edu>
2020-01-17 14:05:03 -0600

    remove flow models

--- app/javascript/packs/channels/PlaylistChannel.jsx
+++ app/javascript/packs/channels/PlaylistChannel.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 
-import { defaultSong } from '../models'
+import { defaultSong } from '../models/Song'
 
 function withPlaylistChannel (
   Component
--- app/javascript/packs/models/Song.js
+++ app/javascript/packs/models/Song.js
@@ -1,21 +1,3 @@
-/**
- * @providesModule Song
- * @flow
- */
-
-export type Song = {
-  +id: ?number,
-  +name: string,
-  +artist: string,
-  +album: string,
-  +label: string,
-  +year: ?number,
-  +request: boolean,
-  +new: boolean,
-  +local: boolean,
-  +at: Date,
-}
-
 export const defaultSong: Song = {
   id: null,
   name: '',
